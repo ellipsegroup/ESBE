c524531467c2736657a6827d854363c33a21fbc9
cis.dev764 <shikhar.s@cisinlabs.com>
2019-06-04 20:30:17 +0530

    import 1000-1500 articles

--- netimpex_odoo_sync/models/models.py
+++ netimpex_odoo_sync/models/models.py
@@ -84,9 +84,9 @@ class Product(models.Model):
         get_product_response = requests.get(get_product_url)
         product_data = get_product_response.json()
         logger.info("-----------------------total products "+str(len(product_data)))
-        logger.info("-----------------------import first - 1500 - last")
+        logger.info("-----------------------import - 1000 - 1500")
         
-        for index, product in enumerate(product_data[1500:]):
+        for index, product in enumerate(product_data[1000:1500]):
             art_id = product.get('article_id')
             product_id = self.env['product.product'].search([('netimpex_product_id', '=', art_id)])
             TimestampUtc = product['article_create_date']
