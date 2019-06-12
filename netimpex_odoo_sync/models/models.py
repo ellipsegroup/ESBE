cc0195feda12e9e8641eaf554b710e61e53413a9
cis.dev764 <shikhar.s@cisinlabs.com>
2019-06-12 22:06:14 +0530

    import 1500 to all products with all details

--- netimpex_odoo_sync/models/models.py
+++ netimpex_odoo_sync/models/models.py
@@ -111,9 +111,9 @@ class Product(models.Model):
         get_product_response = requests.get(get_product_url)
         product_data = get_product_response.json()
         logger.info("-----------------------total products "+str(len(product_data)))
-        logger.info("-----------------------import - 1300 - 1500")
+        logger.info("-----------------------import - 1500 - all")
         
-        for index, product in enumerate(product_data[1300:1500]):
+        for index, product in enumerate(product_data[1500:]):
             art_id = product.get('article_id')
             product_id = self.env['product.product'].search([('netimpex_product_id', '=', art_id)])
             TimestampUtc = product['article_create_date']
